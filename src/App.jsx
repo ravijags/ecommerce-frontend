f1294e54304c50a4ef700048f43e832c77130371
ravijags <rvjags007@gmail.com>
2026-06-19 18:23:15 +0530

    add react router -home, login, register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Header from './Header'
-import ProductCard from './ProductCard'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   const [cartCount, setCartCount] = useState(0)
-  const [products, setProducts] = useState([])
 
-  useEffect(() => {
-    fetch("https://ecommerce-v2-y8jy.onrender.com/api/products")
-      .then(res => res.json())
-      .then(data => setProducts(data.products))
-  }, [])
-  
   return (
-    <div className='bg-gray-100 min-h-screen'>
+    <div className="bg-gray-100 min-h-screen">
       <Header cartCount={cartCount} />
-      <main className='max-w-6xl mx-auto px-6 py-8'>
-        <h2 className='text-2xl font-bold text-gray-800 mb-6'>Products</h2>
-        <div className='flex flex-wrap gap-6'>
-          {products.map(product => (
-            <ProductCard
-              key={product.id}
-              name={product.name}
-              price={product.price}
-              description={product.description}
-              image={product.image}
-              onAddToCart={() => setCartCount(cartCount + 1)} 
-            />
-          ))}
-
-        </div>
-
-      
-      </main>
+      <Routes>
+        <Route path="/" element={<Home cartCount={cartCount} setCartCount={setCartCount} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+    return (
+      <main className="max-w-md mx-auto px-6 py-8">
+      <h2 className="text-2xl font-bold text-gray-800 mb-6">Login</h2>
+      <p>Login page coming soon!</p>
+    </main>
+    )
+}
+
+export default Login
